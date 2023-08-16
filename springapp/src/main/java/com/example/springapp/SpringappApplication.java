1c633e97d3fbdf5d25cc51b4649fe0c860bf6afe
Iamneo <techadmin@iamneo.ai>
2023-08-16 14:33:33 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @RestController
 @RequestMapping("/api/v1/user")
 public class SpringappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringappApplication.class, args);
+      @GetMapping("/hello")
+      public   
+		
 	}
 
 }
